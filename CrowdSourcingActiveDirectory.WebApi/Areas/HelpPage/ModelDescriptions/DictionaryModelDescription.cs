09f9edf27a7e0bd00f6362a45677ccce7204146d
internitys <aminearbi@live.com>
2015-02-21 19:03:18 +0100

    FirstCommit

---
+++ CrowdSourcingActiveDirectory.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CrowdSourcingActiveDirectory.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CrowdSourcingActiveDirectory.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CrowdSourcingActiveDirectory.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
